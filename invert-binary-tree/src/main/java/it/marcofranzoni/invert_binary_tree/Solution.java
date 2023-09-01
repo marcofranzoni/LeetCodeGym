e278e1d69714c484d3f7e8bd3e2ea1a808abee9e
Marco Franzoni <marco.franzoni@ynap.com>
2023-09-01 19:01:34 +0200

    simplified logic
    remove unneeded check

--- invert-binary-tree/src/main/java/it/marcofranzoni/invert_binary_tree/Solution.java
+++ invert-binary-tree/src/main/java/it/marcofranzoni/invert_binary_tree/Solution.java
@@ -1,25 +1,16 @@
 package it.marcofranzoni.invert_binary_tree;
 
 public class Solution {
-    public TreeNode invertTree(TreeNode root) {
-
+	public TreeNode invertTree(TreeNode root) {
 		return recursiveInvert(root);
-    }
+	}
 
-    private TreeNode recursiveInvert(TreeNode node) {
-        if (node == null) {
-            return null;
-        }
+	private TreeNode recursiveInvert(TreeNode node) {
+		if (node == null) {
+			return null;
+		}
 
-        if (node.left != null || node.right != null) {
-            TreeNode temp = node.left;
-            node.left = recursiveInvert(node.right);
-            node.right = recursiveInvert(temp);
-        }
-
-        return node;
-
-    }
-
+		return new TreeNode(node.val, recursiveInvert(node.right), recursiveInvert(node.left));
+	}
 
 }
