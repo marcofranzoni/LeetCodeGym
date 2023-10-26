171f9d7926fb30d54ce7bf896460d98eabcefd9c
Marco Franzoni <marco.franzoni@ynap.com>
2023-10-26 22:02:12 +0200

    improved readability

--- valid-parentheses/src/main/java/it/marcofranzoni/valid_parentheses/Solution.java
+++ valid-parentheses/src/main/java/it/marcofranzoni/valid_parentheses/Solution.java
@@ -1,8 +1,11 @@
 package it.marcofranzoni.valid_parentheses;
 
+import java.util.Map;
 import java.util.Stack;
 
 public class Solution {
+
+	private final Map<Character, Character> PARENTHESES = Map.of(')', '(', ']', '[', '}', '{');
 	public boolean isValid(String s) {
 
 		if (s.length() < 2) {
@@ -12,26 +15,10 @@ public class Solution {
 		Stack<Character> input = new Stack<>();
 
 		for (char c : s.toCharArray()) {
-			if (c == ')') {
+			if (PARENTHESES.containsKey(c)) {
 				if (!input.isEmpty()) {
 					char lastParentheses = input.pop();
-					if (lastParentheses != '(')
-						return false;
-				} else {
-					return false;
-				}
-			} else if (c == ']') {
-				if (!input.isEmpty()) {
-					char lastParentheses = input.pop();
-					if (lastParentheses != '[')
-						return false;
-				} else {
-					return false;
-				}
-			} else if (c == '}') {
-				if (!input.isEmpty()) {
-					char lastParentheses = input.pop();
-					if (lastParentheses != '{')
+					if (lastParentheses != PARENTHESES.get(c))
 						return false;
 				} else {
 					return false;
@@ -45,4 +32,5 @@ public class Solution {
 		return input.isEmpty();
 
 	}
+
 }
